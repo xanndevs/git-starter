bc68cb31eb4916764b3715bb13b8f93c1bc5643d
xanndev <canberkcakir04@gmail.com>
2023-11-15 21:25:30 +0300

    Initial commit. Added the base functionality.

---
+++ main.java
@@ -0,0 +1,18 @@
+public class Tester{
+    public static void main(String args[]){
+        System.out.println("Less gooooo!!!");
+    }
+
+    public int topla(int x, int y){
+        return x+y;
+    }
+    public int cikar(int x, int y){
+        return x-y;
+    }
+    public int carp(int x, int y){
+        return x*y;
+    }
+    public float bol(int x, int y){
+        return x/y;
+    }
+}
